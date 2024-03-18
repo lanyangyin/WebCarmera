ca37f63a1ed6114bca0c28dcf8439dafd34049e4
lanan <2436725966@qq.com>
2024-03-19 06:50:46 +0800

    增加了屏幕共享，为摄像头添加了旋转

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-import cv2
-from flask import Flask, redirect, request, render_template, Response
-from flask_sslify import SSLify
+import socket
 import ssl
 
+import cv2
+import numpy as np
+import pyautogui
+from flask import Flask, redirect, request, render_template, Response
+from flask_sslify import SSLify
+
+host = socket.gethostbyname(socket.gethostname())
 app = Flask(__name__)
 sslify = SSLify(app)
+
+
+@app.before_request
+def force_https():
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
 
 
 @app.route('/')
@@ -37,9 +49,31 @@ def server_():
     return render_template('server/index.html', selected_camera=camera_index, camera_rotate=camera_rotate)  # 将相机列表传递到模板中
 
 
+def screen():
+    while True:
+        # 截屏
+        screenShotImg = pyautogui.screenshot()
+        screenShotImg = cv2.cvtColor(np.array(screenShotImg), cv2.COLOR_RGB2BGR)
+        # 获取鼠标位置
+        mouseX, mouseY = pyautogui.position()
+        # 在图像上绘制鼠标
+        cv2.circle(screenShotImg, (mouseX, mouseY), 5, (0, 255, 0), -1)
+        # 转换图像格式并发送给前端
+        _, frame = cv2.imencode('.JPEG', screenShotImg)
+        frame = frame.tobytes()
+        yield (b'--frame\r\n Content-Type: image/jpeg\r\n\r\n' + frame)
+
+
+@app.route('/screen_sharing')
+def video_fed():
+    # 分帧推送截屏图像数据到前端
+    return Response(screen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 @app.route('/client')
 def client():
-    return render_template('client/index.html')
+    camera_rotate = request.args.get('r', default='0', type=int)
+    return render_template('client/index.html', camera_rotate=camera_rotate)
 
 
 @app.route('/favicon.ico')
@@ -47,14 +81,7 @@ def favicon():
     return Response('', status=200)
 
 
-@app.before_request
-def force_https():
-    if not request.is_secure:
-        url = request.url.replace('http://', 'https://', 1)
-        return redirect(url, code=301)
-
-
 if __name__ == '__main__':
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     context.load_cert_chain('./cert.pem', './key.pem')  # 替换为你的证书和私钥路径
-    app.run(host='localhost', port=2040, ssl_context=context, debug=True)
+    app.run(host=host, port=2024, ssl_context=context, debug=True)
